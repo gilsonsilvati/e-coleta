952c9c9ff46ed7013985303fa388f3336b1f6d3f
Gilson Silva <gilsonsilvati@gmail.com>
2020-06-05 02:11:51 -0300

    Iniciando nossa API Rest

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,42 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-      console.log('Listagem de usuário');
+app.use(express.json());
 
-      response.json([
-            'Gilson',
-            'Lidy',
-            'Leka',
-            'Biel',
-            'Test'
-      ]);
+const users = [
+      'Gilson',
+      'Lidy',
+      'Leka',
+      'Biel',
+      'Test'
+];
+
+app.get('/users', (request, response) => {
+      const search = String(request.query.search);
+
+      const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+      return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+      const id = Number(request.params.id);
+
+      const user = users[id];
+
+      return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+      const data = request.body;
+
+      const user = {
+            name: data.name,
+            email: data.email
+      };
+
+      return response.json(user);
 });
 
 app.listen(3333);
