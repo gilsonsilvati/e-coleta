81c643504dcc6ca86cc1e35617779c6337bde33a
Gilson Silva <gilsonsilvati@gmail.com>
2020-06-05 17:18:40 -0300

    Configurando datasource com banco SQLite3

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # e-coleta
 
 ## // TODO: detalhar readme
+
+Tecnologias:
+- NodeJS: Back-End
+- React: Front-End
+- SQLite3: Banco de Dados
+
+Ferramentas:
+- Visual Studio Code: IDE
+- Fork: Versionamento com GIT
+- Insomnia: Client Rest
+
+Comandos:
+- npx knex migrate:latest --knexfile knexfile.ts migrate:latest
+: Gerar entity
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,41 +1,10 @@
-import express, { request, response } from 'express';
+import express from 'express';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-const users = [
-      'Gilson',
-      'Lidy',
-      'Leka',
-      'Biel'
-];
-
-app.get('/users', (request, response) => {
-      const search = String(request.query.search);
-
-      const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-      return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-      const id = Number(request.params.id);
-
-      const user = users[id];
-
-      return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-      const data = request.body;
-
-      const user = {
-            name: data.name,
-            email: data.email
-      };
-
-      return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
